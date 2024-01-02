3cc1b08535a67667bf422a29f89dc5786b081281
Aryan Bhati <123343302+AryanBhati7@users.noreply.github.com>
2024-01-02 17:02:41 +0530

    Add files via upload

---
+++ Day 2/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    public static void main(String args[]){
+        byte a = 5;
+        System.out.println(a);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = false;
+        System.out.println(var);
+        int number = 25;
+        System.out.println(number);
+        long num = 15876;
+        System.out.println(num);
+        double num2 = 2168;
+        System.out.println(num2);
+    }
+}
