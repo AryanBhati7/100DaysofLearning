0d8a4dfc102accd8c4c7774ac423893c0e0cf139
Aryan Bhati <123343302+AryanBhati7@users.noreply.github.com>
2024-01-10 22:29:43 +0530

    Add files via upload

---
+++ Day 9/loop1.java
@@ -0,0 +1,19 @@
+public class loop1 {
+    public static void main(String args[]){
+        //WHILE Loop
+        int count = 1;
+        while(count<=100){
+            System.out.println("Hello World");
+            count++;
+        }
+        //FOR-LOOP
+        for(int i=1; i<=100; i++){
+            System.out.println("Hello World");
+        }
+        //Do-While Loop
+        int count2 = 1;
+        do{
+            System.out.println("Hello world");
+        }while(count2<=100);
+    }
+}
