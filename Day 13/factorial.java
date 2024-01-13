3e2e36611593e41a098d1dcd26ed4f01f948eb21
Aryan Bhati <123343302+AryanBhati7@users.noreply.github.com>
2024-01-13 20:37:09 +0530

    added some comment factorial.java

--- Day 13/factorial.java
+++ Day 13/factorial.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class factorial {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-
+        //Takes input from the user
         System.out.print("Enter the number : ");
         long n = sc.nextLong();
-
+        //Loop to calculate the factorial
         long fac=1;
         for(long i=n;i>1; i--){
             fac = fac*i;
