9e6033d752a9b7734fd003445f77c02faa7c641f
Aryan Bhati <123343302+AryanBhati7@users.noreply.github.com>
2024-02-02 22:59:46 +0530

    Add files via upload

---
+++ Day 33/Zero_One_Triangle.java
@@ -0,0 +1,18 @@
+public class Zero_One_Triangle {
+    public static void Triangleof01 (int n){
+        for  (int i = 1; i <= n; i++){
+            for (int j = 1; j <= i; j++){
+                if((i+j)%2==0){
+                    System.out.print("1 ");
+                }
+                else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        Triangleof01(5);
+    }
+}
