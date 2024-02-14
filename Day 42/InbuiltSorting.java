f0f380a86b2c91277188cb6ef8dce4206ecd339e
Aryan Bhati <123343302+AryanBhati7@users.noreply.github.com>
2024-02-14 22:22:24 +0530

    Add files via upload

---
+++ Day 42/InbuiltSorting.java
@@ -0,0 +1,22 @@
+//import java.util.Arrays;
+//import java.util.Collections;
+//Saare functions ko import ke liye --- import java.util.*; 
+public class InbuiltSorting {
+    public static void PrintArr(Integer arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + "  ");
+        }
+        System.out.println();
+    }
+    // public static void main(String args[]){
+    //     Integer arr[] = {5,4,1,3,2};
+    //     Arrays.sort(arr,0,3);
+    //     PrintArr(arr);
+    //     Arrays.sort(arr);
+    //     PrintArr(arr);
+    //     Arrays.sort(arr,0,3,Collections.reverseOrder());
+    //     PrintArr(arr);
+    //     Arrays.sort(arr,Collections.reverseOrder());
+    //     PrintArr(arr);
+    // }
+}
