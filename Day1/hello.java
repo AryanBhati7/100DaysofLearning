8ad4b1f0698cc3724d6a14f5f72eb3e0b10ed155
Aryan Bhati <aryanbhati9638@gmail.com>
2024-02-18 20:02:49 +0530

    minor rename

--- Day1/hello.java
+++
@@ -1,9 +0,0 @@
-public class hello {
-    public static void main(String args[]){
-        System.out.println("Hello World");
-
-        int $ = 24;
-        System.out.println($);
-    }
-}
-//boiler plate code
