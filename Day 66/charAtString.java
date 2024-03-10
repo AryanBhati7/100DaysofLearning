9b95e6bf6f7b18b15dcf448ee14aa1c1607de2dd
Aryan Bhati <aryanbhati9638@gmail.com>
2024-03-10 12:54:08 +0530

    Day 67 file

---
+++ Day 66/charAtString.java
@@ -0,0 +1,11 @@
+public class charAtString {
+    public static void PrintCharsinString(String str){
+        for(int i = 0; i<str.length(); i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+    public static void main(String args[]){
+        String name = "ARYAN";
+        PrintCharsinString(name);
+    }
+}
---
+++ Day 67/Substring.java
@@ -0,0 +1,14 @@
+public class Substring {
+    public static String getSubstring(String str, int si, int ei){
+        String subStr = "";
+        for(int i = si; i < ei; i++){
+            subStr += str.charAt(i);
+        }
+        return subStr;
+    }
+    public static void main(String args[]){
+        String str = "Hello World";
+            System.out.println(getSubstring(str, 0, 5)); //UserBuilt Function
+            System.out.println(str.substring(0,5)); //Inbuilt Function
+    }
+}
