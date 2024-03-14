d76b87cc12da2cd7ba23b1aaaed2f5b0e6b11650
Aryan Bhati <aryanbhati9638@gmail.com>
2024-03-14 21:37:20 +0530

    fix : Day 73

--- Day 73/ShortestPath.java
+++ Day 73/ShortestPath.java
@@ -1,29 +1,24 @@
 public class ShortestPath {
-    public static double getShortestPath(String Route){
+    public static double getShortestPath(String Route) {
         int x = 0;
         int y = 0;
-        for(int i = 0; i < Route.length(); i++){
-            // switch(Route.charAt(i)){
-            //     case 'N' : 
-            // }
-            if(Route.charAt(i)=='N'){
+        for (int i = 0; i < Route.length(); i++) {
+            if (Route.charAt(i) == 'N') {
                 y += 1;
-            }
-            else if(Route.charAt(i)=='S'){
+            } else if (Route.charAt(i) == 'S') {
                 y -= 1;
-            }
-            else if(Route.charAt(i)=='E'){
+            } else if (Route.charAt(i) == 'E') {
                 x += 1;
-            }
-            else{
+            } else {
                 x -= 1;
             }
         }
-        double ShortestPath = (int) Math.sqrt((Math.pow(x,2) + Math.pow(y,2)));
+        double ShortestPath = (int) Math.sqrt((Math.pow(x, 2) + Math.pow(y, 2)));
         return ShortestPath;
     }
-    public static void main(String args[]){
-        //String path = "WNEENESENNN";
+
+    public static void main(String args[]) {
+        // String path = "WNEENESENNN";
         String path = "NS";
         System.out.println("SHORTEST PATH : " + getShortestPath(path));
     }
