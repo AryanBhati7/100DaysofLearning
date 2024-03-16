a3596566e6aa9b6d0eed070460e6e6461e2f7315
Aryan Bhati <aryanbhati9638@gmail.com>
2024-03-16 13:05:52 +0530

    add : Day 75

---
+++ Day 75/Bitwise_Operators.java
@@ -0,0 +1,15 @@
+public class Bitwise_Operators {
+    public static void main(String args[]){
+        int a  = 5, b = 6;
+        //AND Operator
+        System.out.println(a&b);
+        //OR Operator
+        System.out.println(a|b);
+        //XOR Operator
+        System.out.println(a^b);
+        //NOT Operator - Binary One's Complement
+        System.out.println(~a + "  " + ~b);
+        //Left Shift Operator
+        System.out.println(a<<2);
+    }
+}
