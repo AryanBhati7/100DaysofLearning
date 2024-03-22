0797f99970415d8bdab989db74a642749ad56b6a
Aryan Bhati <aryanbhati9638@gmail.com>
2024-03-22 08:40:20 +0530

    add : Day 80

---
+++ Day 81/Set_ith_bit.java
@@ -0,0 +1,9 @@
+public class Set_ith_bit {
+    public static int SetIthBit(int n, int i){
+        int bitmask = 1<<i;
+        return n|bitmask;
+    }
+    public static void main(String args[]){
+        System.out.println(SetIthBit(10, 2));
+    }
+}
