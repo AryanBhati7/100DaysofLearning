f6d36e2aaec97952123752d81f31ea24dcdf3f1e
Aryan Bhati <aryanbhati9638@gmail.com>
2024-04-03 22:42:59 +0530

    add : Day 91

---
+++ Day 91/LengthofString.java
@@ -0,0 +1,12 @@
+public class LengthofString {
+    public static int length(String str){
+        if(str.length()==0){
+            return 0;
+        }
+        return length(str.substring(1))+1;
+
+    }
+    public static void main(String args[]){
+        System.out.println(length("Aryan"));
+    }
+}
